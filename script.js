dc82a80cd23b636a9261644f4f9f88c33392a45b
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2025-11-19 14:38:01 +0530

    Script added

---
+++ script.js
@@ -0,0 +1,30 @@
+// // let janhavi = 12;
+// // console.log(janhavi);
+
+// // let a = 4;
+// // let b = 2;
+// // console.log(a ** b);
+// // let Surname = "Mishra";
+// // let Student = {
+// //   Name: "Janhavi",
+// //   surname: Surname,
+// //   Address: "68",
+// //   marks: 78,
+// //   Ispass: true,
+// //   67: 31,
+// // };
+
+// // console.log(Student.surname, Student.Naratingme, Student.Ispass);
+// let product = {
+//   name: "parker",
+//   mrp: 270,
+//   rating: 4,
+//   numbers: 7002,
+// };
+
+let num = prompt("Enter a Number"); // String
+let num2 = prompt("Enter Second Number"); //String
+let newNum = parseInt(num); //String To interger conversion
+let newnum2 = parseInt(num2); //String To interger conversion
+console.log("The sum of your given number are", newNum + newnum2);
+console.log("The sum of your given number are", num + num2);
