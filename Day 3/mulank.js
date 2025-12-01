8e422339fb9039ef3fadf0bc21598b60ad9583de
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2025-12-01 15:47:36 +0530

    mulank question

---
+++ Day 3/mulank.js
@@ -0,0 +1,24 @@
+// function mulank(userdate) {
+//   if (inputvalidate(userdate)) {
+//     let num1 = parseInt(userdate[0]);
+//     let num2 = parseInt(userdate[1]);
+//     let remainder = num1 + num2;
+//     if (remainder >= 10) {
+//       mulank(String(remainder));
+//     } else {
+//       console.log(`You mulank is ${remainder}`);
+//     }
+//   } else {
+//     console.log("Enter valid Input");
+//   }
+// }
+// function inputvalidate(input) {
+//   let input1 = parseInt(input);
+//   if (!isNaN(input1) && input1 > 0 && input1 <= 31) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// }
+// let userdate = prompt("Enter your birth date");
+// mulank(userdate);
